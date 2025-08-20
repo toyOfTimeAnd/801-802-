5c8fc1d43216fc9ba3f0ed855a7d864c4750459e
toyOfTimeAnd <vbms@yandex.ru>
2025-08-20 18:52:32 +0700

    Add files via upload

---
+++ final/js/primarchs.js
@@ -0,0 +1,357 @@
+const  primarchs = [
+     {legionNumber: "I", 
+    legionName: "Темные Ангелы",
+    primarchName: "Лев Эль Джонсон",
+     worldOfOrigin: "Калибан",
+    loyality: "Лоялист",
+    liveStatus: "Жив",
+     addLiveStatus: ", очнулся совсем недавно",
+    },
+   
+    {legionNumber: "III", 
+     legionName: "Дети Императора",
+     primarchName: "Фулгрим",
+     worldOfOrigin: "Кемос",
+     loyality: "Трейтор",
+      god: "Слаанеш",
+     liveStatus: "Жив",
+     addLiveStatus: ", правит демоническим миром в Оке Ужаса",
+      },
+   
+    {legionNumber: "IV", 
+       legionName: "Железные Воины",
+       primarchName: "Пертурабо",
+     worldOfOrigin: "Олимпия",
+       loyality: "Трейтор",
+     god: "Хаос Неделимый",
+       liveStatus: "Жив",
+     addLiveStatus: ", демон-принц, владеет миром Медренгард в Оке Ужаса",
+       },
+   
+    {legionNumber: "V", 
+       legionName: "Белые Шрамы",
+       primarchName: "Джагатай Хан",
+     worldOfOrigin: "Чогорис (Мундус Планус)",
+       loyality: "Лоялист",
+       liveStatus: "Считается пропавшим",
+     addLiveStatus: "",
+       },
+   
+    {legionNumber: "VI", 
+       legionName: "Космические волки",
+       primarchName: "Леман Русс ",
+     worldOfOrigin: "Фенрис",
+        loyality: "Лоялист",
+       liveStatus: "Считается пропавшим",
+     addLiveStatus: ", иногда является в видениях своим сынам",
+       },
+   
+    {legionNumber: "VII", 
+       legionName: "Имперские кулаки",
+       primarchName: "Рогал Дорн",
+     worldOfOrigin: "Инвит",
+       loyality: "Лоялист",
+       liveStatus: "Мертв:", 
+     addLiveStatus: " убит во время боя на палубе линкора «Меч Святотатства» во время первого Черного Крестового Похода",
+       },
+   
+    {legionNumber: "VIII", 
+       legionName: "Повелители ночи",
+       primarchName: "Конрад Кёрз",
+     worldOfOrigin: "Нострамо",
+       loyality: "Трейтор",
+     god: "Хаос Неделимый",
+       liveStatus: "Мертв:", 
+              addLiveStatus: "Позволил себя убить ассасину Храма Каллидуса, М’Шен",
+       },
+   
+    {legionNumber: "IX", 
+       legionName: "Кровавые Ангелы",
+       primarchName: "Сангвиний",
+     worldOfOrigin: "Баал Секундус",
+       loyality: "Лоялист",
+       liveStatus: "Мертв:", 
+              addLiveStatus: " убит Хорусом",
+       },
+   
+    {legionNumber: "X", 
+       legionName: "Железные Руки",
+       primarchName: "Феррус Манус",
+     worldOfOrigin: "Медуза",
+       loyality: "Лоялист",
+       liveStatus: "Мертв:", 
+              addLiveStatus: " убит Фулгримом",
+       },
+ 
+ 
+   {legionNumber: "XII", 
+     legionName: "Пожиратели Миров",
+     primarchName: "Ангрон",
+   worldOfOrigin: "Нуцерия",
+     loyality: "Трейтор",
+    god: "Кхорн",
+     liveStatus: "Жив", 
+            addLiveStatus: ", демон-принц, изгнан в варп (не оставляет попыток прорваться в реальность)",
+     },
+ 
+   {legionNumber: "XIII", 
+     legionName: "Ультрамарины",
+     primarchName: "Робаут Жиллиман",
+   worldOfOrigin: "Макрагг",
+     loyality: "Лоялист",
+     liveStatus: "Жив", 
+            addLiveStatus: ", был тяжело ранен Фулгримом, долго находился в стазисе, по актуальному лору из него вышел и пытается разгрести все, что накопилось за это время",
+     },
+ 
+   {legionNumber: "XIV", 
+     legionName: "Гвардия Смерти",
+     primarchName: "Мортарион",
+   worldOfOrigin: "Барбарус",
+     loyality: "Трейтор",
+    god: "Нургл",
+     liveStatus: "Жив", 
+            addLiveStatus: ", демон-принц, правит Чумной планетой в Оке Ужаса",
+     },
+ 
+   {legionNumber: "XV", 
+     legionName: "Тысяча сынов",
+     primarchName: "Магнус Красный (Рыжий)",
+   worldOfOrigin: "Просперо",
+     loyality: "Трейтор",
+    god: "Тзинч",
+     liveStatus: "Жив", 
+            addLiveStatus: ", демон-принц, правит планетой Волшебников в Оке Ужаса",
+     },
+ 
+   {legionNumber: "XVI", 
+     legionName: "Лунные Волки, Сыны Хоруса, Черный легион",
+     primarchName: "Хорус Луперкаль",
+   worldOfOrigin: "Хтония",
+     loyality: "Трейтор",
+    god: "Хаос Неделимый",
+     liveStatus: "Мертв:", 
+            addLiveStatus: " убит Императором",
+     },
+ 
+   {legionNumber: "XVII", 
+     legionName: "Несущие слово",
+     primarchName: "Лоргар Аврелиан",
+   worldOfOrigin: "Колхис",
+     loyality: "Трейтор",
+        god: "Хаос Неделимый",
+     liveStatus: "Жив", 
+            addLiveStatus: ", демон-принц, правит миром Сикарус в Оке Ужаса",
+     },
+ 
+   {legionNumber: "XVIII", 
+     legionName: "Саламандры",
+     primarchName: "Вулкан",
+   worldOfOrigin: "Ноктюрн",
+     loyality: "Лоялист",
+     liveStatus: "Считается пропавшим",
+       addLiveStatus: " во время Резни на Истваане V",
+     },
+ 
+   {legionNumber: "XIX", 
+     legionName: "Гвардия Ворона",
+     primarchName: "Корвус Коракс",
+   worldOfOrigin: "Освобождение (первоначально-Ликей, луна Киавара)",
+     loyality: "Лоялист",
+     liveStatus: "Считается пропавшим", 
+            addLiveStatus: ". Судя по всему странствует по Оку Ужаса, пытаясь выручить своих воинов",
+     },
+   
+   {legionNumber: "XX", 
+     legionName: "Альфа Легион",
+     primarchName: "Альфарий и Омегон",
+   worldOfOrigin: "неизвестен",
+     loyality: "Трейтор",
+     liveStatus: "Мертв:", 
+            addLiveStatus: " \nОмегон – убит Рогалом Дорном на Плутоне*, \nАльфарий – убит Жиллиманом на Эскрадоре*  \n* Возможно",
+     },
+   
+ ]
+
+ function searchByName() {
+        const input = document.getElementById('searchInput').value.trim();
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = ''; 
+
+        if (input === '') {
+            resultDiv.innerText = 'Здесь надо написать имя примарха';
+            return;
+        }
+
+        let result = '';
+        let found = false;
+
+        for (let i = 0; i < primarchs.length; i++) {
+            if (primarchs[i].primarchName.toLowerCase().includes(input.toLowerCase())) {
+                found = true;
+                result += primarchs[i].legionNumber + " " + primarchs[i].legionName + " \n" + 
+                          primarchs[i].primarchName + " \n" + 
+                          primarchs[i].loyality + "\n" + 
+                          primarchs[i].liveStatus + " " + primarchs[i].addLiveStatus + "\n";
+            }
+        }
+
+        if (found) {
+            resultDiv.innerText = 'Вот твой красавец:\n' + result;
+        } else {
+            resultDiv.innerText = 'Таких тут нет';
+        }
+    }
+
+    document.getElementById('searchButton').addEventListener('click', searchByName);
+      
+
+    
+    
+ function searchAll() {
+            let result = '';
+            for (let i = 0; i < primarchs.length; i++) {
+                {result += primarchs[i].legionNumber  + " " + primarchs[i].legionName + " \n " + 
+                primarchs[i].primarchName + " \n" + 
+                primarchs[i].loyality + "\n " + 
+                primarchs[i].liveStatus + primarchs[i].addLiveStatus+ "\n " ;
+ } 
+ }
+            
+ const displayDiv = document.getElementById('Все сразу');
+
+    if (!isVisible) {
+        displayDiv.innerText = result;
+        isVisible = true;
+    } else {
+        displayDiv.innerText = '';
+        isVisible = false;
+    }
+}
+
+document.getElementById('button').addEventListener('click', searchAll);
+
+
+ function searchLoyalists() {
+    let result = '';
+    for (let i = 0; i < primarchs.length; i++) {
+        if (primarchs[i].loyality === "Лоялист") {
+            result += primarchs[i].legionNumber + " " + primarchs[i].legionName + " \n" +
+                      primarchs[i].primarchName + " \n" +
+                      primarchs[i].loyality + "\n" +
+                      primarchs[i].liveStatus + " " + primarchs[i].addLiveStatus + "\n";
+        }
+    }
+    
+    const displayDiv = document.getElementById('ЛОЯЛИСТЫ');
+    
+    if (!isVisible) {
+        displayDiv.innerText = result;
+        isVisible = true;
+    } else {
+        displayDiv.innerText = '';
+        isVisible = false;
+    }
+}
+
+document.getElementById('button1').addEventListener('click', searchLoyalists);
+
+let isVisible = false;
+    function searchTraitors() {
+        let result = '';
+ for (let i = 0; i < primarchs.length; i++) {
+  if (primarchs[i].loyality === "Трейтор") {
+    result+= primarchs[i].legionNumber  + " " + primarchs[i].legionName + " \n " + 
+    primarchs[i].primarchName + " \n" + 
+    primarchs[i].loyality + "\n " + 
+    primarchs[i].liveStatus + primarchs[i].addLiveStatus+ "\n";
+ } 
+ }
+ 
+ const displayDiv = document.getElementById('ТРЕЙТОРЫ');
+    
+    if (!isVisible) {
+        displayDiv.innerText = result;
+        isVisible = true;
+    } else {
+        displayDiv.innerText = '';
+        isVisible = false;
+    }
+}
+
+document.getElementById('button2').addEventListener('click', searchTraitors);
+
+
+function searchAlive() {
+        let result = '';
+ for (let i = 0; i < primarchs.length; i++) {
+  if (primarchs[i].liveStatus === "Жив") {
+    result+= primarchs[i].legionNumber  + " " + primarchs[i].legionName + " \n " + 
+    primarchs[i].primarchName + " \n" + 
+    primarchs[i].loyality + "\n " + 
+    primarchs[i].liveStatus + primarchs[i].addLiveStatus+ "\n";
+ } 
+ }
+ const displayDiv = document.getElementById('ЖИВЫЕ');
+    
+    if (!isVisible) {
+        displayDiv.innerText = result;
+        isVisible = true;
+    } else {
+        displayDiv.innerText = '';
+        isVisible = false;
+    }
+}
+
+document.getElementById('button3').addEventListener('click', searchAlive);
+
+
+function searchDead() {
+        let result = '';
+ for (let i = 0; i < primarchs.length; i++) {
+  if (primarchs[i].liveStatus === "Мертв:") {
+    result+= primarchs[i].legionNumber  + " " + primarchs[i].legionName + " \n " + 
+    primarchs[i].primarchName + " \n" + 
+    primarchs[i].loyality + "\n " + 
+    primarchs[i].liveStatus + primarchs[i].addLiveStatus+ "\n";
+ } 
+ }
+ 
+ const displayDiv = document.getElementById('МЕРТВЫЕ');
+    
+    if (!isVisible) {
+        displayDiv.innerText = result;
+        isVisible = true;
+    } else {
+        displayDiv.innerText = '';
+        isVisible = false;
+    }
+}
+
+document.getElementById('button4').addEventListener('click', searchDead);
+
+
+function searchMissing() {
+        let result = '';
+ for (let i = 0; i < primarchs.length; i++) {
+  if (primarchs[i].liveStatus === "Считается пропавшим") {
+    result+= primarchs[i].legionNumber  + " " + primarchs[i].legionName + " \n " + 
+    primarchs[i].primarchName + " \n" + 
+    primarchs[i].loyality + "\n " + 
+    primarchs[i].liveStatus + primarchs[i].addLiveStatus + "\n ";
+ } 
+ }
+ const displayDiv = document.getElementById('ПРОПАВШИЕ БЕЗ ВЕСТИ');
+    
+    if (!isVisible) {
+        displayDiv.innerText = result;
+        isVisible = true;
+    } else {
+        displayDiv.innerText = '';
+        isVisible = false;
+    }
+}
+
+document.getElementById('button5').addEventListener('click', searchMissing);
+ 
+ 
+
